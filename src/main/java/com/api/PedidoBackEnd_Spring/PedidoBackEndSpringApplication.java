4a446bf0376ab61407d6f1b2fc5411fcd3c601f9
janildoss <janildoucsalinfo@gmail.com>
2023-08-10 13:45:27 -0300

    Endpoint Categoria, config. e teste banco H2

--- src/main/java/com/api/PedidoBackEnd_Spring/PedidoBackEndSpringApplication.java
+++ src/main/java/com/api/PedidoBackEnd_Spring/PedidoBackEndSpringApplication.java
@@ -1,5 +1,6 @@
 package com.api.PedidoBackEnd_Spring;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class PedidoBackEndSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PedidoBackEndSpringApplication.class, args);
+		//System.out.println("Hello word");
 	}
 
 }
