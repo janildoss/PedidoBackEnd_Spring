9f9edd51ded5fdd6d46899eed636639168ebad3c
janildoss <janildoucsalinfo@gmail.com>
2023-08-18 17:21:34 -0300

    Cliente, Tipo Cliente, Telefones e Endereço

--- src/main/java/com/api/PedidoBackEnd_Spring/PedidoBackEndSpringApplication.java
+++ src/main/java/com/api/PedidoBackEnd_Spring/PedidoBackEndSpringApplication.java
@@ -10,10 +10,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.api.PedidoBackEnd_Spring.domain.Categoria;
 import com.api.PedidoBackEnd_Spring.domain.Cidade;
+import com.api.PedidoBackEnd_Spring.domain.Cliente;
+import com.api.PedidoBackEnd_Spring.domain.Endereco;
 import com.api.PedidoBackEnd_Spring.domain.Estado;
 import com.api.PedidoBackEnd_Spring.domain.Produto;
+import com.api.PedidoBackEnd_Spring.enums.TipoCliente;
 import com.api.PedidoBackEnd_Spring.repositories.CategoriaRepository;
 import com.api.PedidoBackEnd_Spring.repositories.CidadeRepository;
+import com.api.PedidoBackEnd_Spring.repositories.ClienteRepository;
+import com.api.PedidoBackEnd_Spring.repositories.EnderecoRepository;
 import com.api.PedidoBackEnd_Spring.repositories.EstadoRepository;
 import com.api.PedidoBackEnd_Spring.repositories.ProdutoRepository;
 
@@ -30,6 +35,12 @@ public class PedidoBackEndSpringApplication implements CommandLineRunner {
 	
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 
 	public static void main(String[] args) {
@@ -68,6 +79,19 @@ public class PedidoBackEndSpringApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","36378912377",TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("2735698","988745562"));
+		
+		Endereco e1 = new Endereco(null,"Rua Flores","300","Apto 303","JArdim Pampulha","38220834",cli1,c1);
+		Endereco e2 = new Endereco(null,"Av.Matos","310","sala 453","Centro","3800245734",cli1,c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
 		
 	}
 	
---
+++ src/main/java/com/api/PedidoBackEnd_Spring/repositories/ClienteRepository.java
@@ -0,0 +1,17 @@
+package com.api.PedidoBackEnd_Spring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+
+import com.api.PedidoBackEnd_Spring.domain.Cliente;
+
+//@Repository
+public interface ClienteRepository extends JpaRepository <Cliente,Integer> {
+
+	//Post
+	//Get
+	//Get one
+	//Put
+	//Delete
+	
+}
