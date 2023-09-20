c12851217fac54807c6be0b4a3d57cbaded639cd
janildoss <prjPedido_BackEnd_Spring>
2023-09-20 10:59:59 -0300

    Deletando Categoria com DELETE e criação de exception personalizada

--- src/main/java/com/api/PedidoBackEnd_Spring/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/api/PedidoBackEnd_Spring/resources/exception/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.api.PedidoBackEnd_Spring.services.exception.DataIntegrityException;
 import com.api.PedidoBackEnd_Spring.services.exception.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -16,4 +18,13 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(),System.currentTimeMillis());
 	    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataItegrity(DataIntegrityException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),System.currentTimeMillis());
+	    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
+	
 }
--- src/main/java/com/api/PedidoBackEnd_Spring/services/CategoriaService.java
+++ src/main/java/com/api/PedidoBackEnd_Spring/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.api.PedidoBackEnd_Spring.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.api.PedidoBackEnd_Spring.domain.Categoria;
 import com.api.PedidoBackEnd_Spring.repositories.CategoriaRepository;
+import com.api.PedidoBackEnd_Spring.services.exception.DataIntegrityException;
 import com.api.PedidoBackEnd_Spring.services.exception.ObjectNotFoundException;
 
 
@@ -33,6 +35,16 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos! ");
+		}
+		
+	}
 	
 		
 }
